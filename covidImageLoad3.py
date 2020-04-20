26b8ea78981458b0f20717a37e4b1a44c13f0c85
Thanigaivelan, Oviya <Oviya@gatech.edu>
2020-04-20 18:33:45 -0500

    Add files via upload
    just uncommented a section

--- covidImageLoad3.py
+++ covidImageLoad3.py
@@ -14,23 +14,23 @@ types = []
 for row in meta.iterrows():
     pid = int(row[1][0])
     t = row[1][1]
-#    if("Pneumo" in t):
-#        filename = row[1][2]
-#        view = row[1][3]
-#
-#        #if(view == "PA"):
-#        try:
-#            if not os.path.exists('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i)):
-#                    imageNew = Image.open("pneumonia_large/" + filename)
-#                    #imageNew = image.resize((1000, 1250))
-#                    imageNew.save('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i))
-#                    i += 1
-#                    print('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i))
-#                    image.close()
-#            else:
-#                print("Already Here")
-#        except:
-#            print("could not open: images/" + filename)
+    if("Pneumo" in t):
+        filename = row[1][2]
+        view = row[1][3]
+
+        #if(view == "PA"):
+        try:
+            if not os.path.exists('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i)):
+                    imageNew = Image.open("pneumonia_large/" + filename)
+                    #imageNew = image.resize((1000, 1250))
+                    imageNew.save('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i))
+                    i += 1
+                    print('images_processed_2/{}_{}_{}.png'.format("Pn", pid, i))
+                    image.close()
+            else:
+                print("Already Here")
+        except:
+            print("could not open: images/" + filename)
             
     if("No Finding" in t):
         filename = row[1][2]
