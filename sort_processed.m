980db0d52d7c4551a3c2f23073070e0e2c9e6778
Thanigaivelan, Oviya <Oviya@gatech.edu>
2020-04-19 20:03:14 -0500

    Add files via upload
    updated combined sort code

--- sort_processed.m
+++ sort_processed.m
@@ -2,12 +2,13 @@ clc
 clear
 close all
 
-source = '/Users/oviyat/Desktop/CX/';
+source = '/home/group12/siamese_vgg/';
 
 listings = dir([source,'images_processed_2']);
 c = [];
 h = [];
 p = [];
+u = [];
 clast = {};
 hlast = {};
 plast = {};
@@ -33,7 +34,6 @@ for i = 4:length(listings)
     else
     end
     
-    
 end
 
 %find unique patient
@@ -44,20 +44,67 @@ end
 for i = 1:length(cindx)
     covid = strcat('CO_',num2str(cpatient(i)),'_',clast{1,cindx(i)});
     filename = strcat(source,'images_processed_2/',covid);
+    A = imread(filename);
+    if length(size(A))>2
+        A = A(126:end-125,:,3);
+    else
+        A = A(126:end-125,:);
+    end
     dest = [cdir covid];
-    movefile(filename,dest);
+    imwrite(A,dest);
+    %movefile(filename,dest);
 end
 
 for i = 1:length(hindx)
     healthy = strcat('No_',num2str(hpatient(i)),'_',hlast{1,hindx(i)});
     filename = strcat(source,'images_processed_2/',healthy);
+    A = imread(filename);
+    if length(size(A))>2
+        A = A(126:end-125,:,3);
+    else
+        A = A(126:end-125,:);
+    end
     dest = [hdir healthy];
-    movefile(filename,dest);
+    imwrite(A,dest);
+    %movefile(filename,dest);
 end
 
 for i = 1:length(pindx)
     pneumonia = strcat('Pn_',num2str(ppatient(i)),'_',plast{1,pindx(i)});
     filename = strcat(source, 'images_processed_2/', pneumonia);
+    A = imread(filename);
+    if length(size(A))>2
+        A = A(126:end-125,:,3);
+    else
+        A = A(126:end-125,:);
+    end
     dest = [pdir pneumonia];
-    movefile(filename,dest);
+    imwrite(A,dest);
+    %movefile(filename,dest);
+end
+
+[~,txt,~] = xlsread([source,'covidfigures.xlsx']);
+for i = 1:length(txt)
+    image = txt{i};
+    sep = strsplit(image,'_');
+    if isequal(sep{1},'CO')
+        cdir = [source,'covid_processed/',image];
+        if isfile(cdir)
+            movefile(cdir,[source,'unique_processed/']);
+        else
+        end
+    elseif isequal(sep{1},'No')
+        hdir = [source,'healthy_processed/',image];
+        if isfile(hdir)
+            movefile(hdir,[source,'unique_processed/']);
+        else
+        end
+    elseif isequal(sep{1},'Pn')
+        pdir = [source,'pne_processed/',image];
+        if isfile(pdir)
+            movefile(pdir,[source,'unique_processed/']);
+        else
+        end
+    else
+    end
 end
